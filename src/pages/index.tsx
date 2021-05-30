ab2407784f390f52ec23301bb31ae9fb8706c93c
lvneto <lauro.neto.1995@gmail.com>
2021-05-30 07:38:31 -0300

    front end complet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,58 @@
-import styles from '../styles/home.module.scss'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { stripe } from '../services/stripe'
+import { SubscribeButton } from './components/SubscribeButton'
+import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }:HomeProps ) {
   return (
-    <h1 className= {styles.title}
-    >First Next <span>Project</span></h1>
+    <>
+    <Head>  
+      <title>Home</title>         
+    </Head>
+
+    <main className={ styles.contentContainer }>
+      <section className={ styles.hero }>
+        <span>👏 Hey, Welcome </span>
+        <h1>News About the <span>React</span> world.</h1>
+        <p>
+          Get acess to all the plubications <br />
+          <span>for {product.amount} month</span>
+        </p>
+        <SubscribeButton priceId={product.priceId} />
+      </section>
+
+      <img src="/images/avatar.svg" alt=" Girl Coding"/>
+    </main>
+
+    </>
+
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+
+  const price = await stripe.prices.retrieve('price_1IwSHgAQyA4tjxMnzXpBypRT')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format (price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24hrs
+  }
 }
